36aea0871227aea4e0e95a1777ce5b5c643f4531
Neha Gupta <nehagupta7@deloitte.com>
2014-12-16 15:46:02 +0530

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,7 +22,7 @@ public class Main extends HttpServlet {
   private void showHome(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
     resp.getWriter().print("Hello from Java!");
-	resp.getWriter().print("Hello all");
+	resp.getWriter().print("Hello all all");
   }
 
   private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
